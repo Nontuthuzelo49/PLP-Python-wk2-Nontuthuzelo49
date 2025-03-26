940a58e025f168054494761aac77599f50bbf849
Nontuthuzelo49 <bridgetm6449@gmail.com>
2025-03-27 01:33:04 +0200

    Initial commit

---
+++ lists.py
@@ -0,0 +1,27 @@
+# Creating an empty list
+my_list = []
+
+# Add elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# Insert an element at a specific index
+my_list.insert(1, 15)
+print(my_list)
+
+# Extend the list with another list
+my_list.extend([50, 60, 70])
+print(my_list)
+
+# Remove the last element from the list
+my_list.pop()   
+print(my_list)
+
+# Sort the list
+my_list.sort()  
+print(my_list)
+
+print(my_list[3])
